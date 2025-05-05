084ba48f1cb9ea3fabc9a5b49e111d84b0226e32
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-05 15:56:26 +0530

    list

---
+++ ds/list/ListDemo3.py
@@ -0,0 +1,7 @@
+data = [100,20,30,400,50]
+
+
+while len(data):
+    print(data.pop(0))
+
+    
--- ds/list/listdemo2.py
+++ ds/list/listdemo2.py
@@ -5,3 +5,4 @@ removedElm = users.pop(1) #remove given index element eg 1 is 1st index.
 #users.remove("sachin")
 print("removing...",removedElm)
 print(users)
+
