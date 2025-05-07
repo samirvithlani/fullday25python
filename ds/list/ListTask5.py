7ff03dd4f70f23f2f0ab6a2081b1e220c56f0a00
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-07 15:55:09 +0530

    list stirng

---
+++ ds/list/ListTask5.py
@@ -0,0 +1,9 @@
+names = ["ram","ravi","sam","rahul","raj","ajay","rekha"]
+rlist =[]
+
+for i in names:
+    if i.startswith("r"):
+        rlist.append(i)    
+
+print(rlist)     
+print(len(rlist))   
