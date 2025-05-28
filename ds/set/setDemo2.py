0314face5473038a84efae7a11eaefb1481c9473
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-28 15:12:26 +0530

    users

---
+++ ds/set/setDemo2.py
@@ -0,0 +1,12 @@
+user1 = {"ram","shyam","amit","sumit","kunal"}
+user2 = {"ram","shyam","amit","ajay","seeta"}
+
+
+#x = user1.union(user2)
+#x = user1 | user2
+#x = user1.intersection(user2)
+#x = user1 & user2
+#x = user2.difference(user1)
+#x = user1 - user2
+x = user1.symmetric_difference(user2)
+print(x)
