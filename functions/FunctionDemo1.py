a2efc47182b18a5317a7c71b37bd6571fcfe2c0c
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-04 15:33:32 +0530

    ans

---
+++ functions/FunctionDemo1.py
@@ -0,0 +1,24 @@
+#function is block of statments
+#use of functions: reusability, readability, maintainability
+#udf predefine
+
+# with return with arguments
+# without return with arguments
+# without return without arguments
+# with return without arguments
+'''
+    void demo(){}
+    int sum(int a,int b){}
+'''
+#use def keyword to define function
+
+def test():
+    print("This is a test function")
+    #without return type without param
+
+def add(a,b):
+    print(a+b)    
+
+
+test()  
+add(100,20)  
---
+++ functions/FunctionDemo2.py
@@ -0,0 +1,18 @@
+def add(a,b,c):
+    x = a + b +c
+    return x
+
+
+ans = add(10,20,30)
+#print("ans = ",ans)
+print(f"ans = {ans}")
+    
+
+def average(a,b,c):
+    return (a + b + c)//3
+
+
+ans = average(10,20,30)
+print(f"ans = {ans}")
+
+print(f"ans = {average(100,200,300)}")
