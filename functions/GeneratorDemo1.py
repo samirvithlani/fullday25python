2265450cee05eb018aaec6edf5ff1e997f032d06
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-18 11:50:23 +0530

    generatorr

---
+++ functions/GeneratorDemo1.py
@@ -0,0 +1,39 @@
+# def getData():
+#     return 1
+#     return 2
+
+# x  = getData()
+# print(x)
+
+def getData():
+    print("get Data called........")
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
+    
+
+# x = getData()
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+
+for x in getData():
+    print(x)
+    
+
+
+def getStudents():
+    yield [f"student {i}" for i in range(1,11)]
+    yield [f"student {i}" for i in range(11,21)]    
+    yield [f"student {i}" for i in range(21,31)]
+
+
+for i in getStudents():
+    print(i)    
+    
+    
+#def getStudents(total,batch):    
